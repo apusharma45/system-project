607032b0f3edbcb45e34857c829f601082886bd0
Apu Sharma <arapusharma45@gmail.com>
2026-02-26 21:44:47 +0600

    build: build the authentication and role-based authorization system

---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsEnum, IsString, MinLength } from 'class-validator';
+import { Role } from '../../../generated/prisma/client.js';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsEnum(Role)
+  role: Role;
+}
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles.decorator';
+import { Role } from '../../generated/prisma/client.js';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.DOCTOR)
+  @Get('me')
+  me(@Req() req: any) {
+    return req.user;
+  }
+}
